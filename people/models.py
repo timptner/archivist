bf2bfec4f68bbb0dc447e9a0dfaa4a97d7607c03
Aiven <aiven@timptner.de>
2022-05-17 21:11:19 +0200

    Add faculty to user model

--- people/models.py
+++ people/models.py
@@ -4,7 +4,18 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+    FACULTIES = [
+        ('MB', _('Mechanical Engineering')),
+        ('VST', _('Process and Systems Engineering')),
+        ('EIT', _('Electrical Engineering and Information Technology')),
+        ('IN', _('Computer Science')),
+        ('MA', _('Mathematics')),
+        ('NW', _('Natural Sciences')),
+        ('ME', _('Medicine')),
+        ('HW', _('Humanities, Social Sciences and Education')),
+        ('WW', _('Economics and Management')),
+    ]
+    faculty = models.CharField(_('Faculty'), max_length=3, choices=FACULTIES, null=True)
 
     class Meta:
         verbose_name = _('User')
